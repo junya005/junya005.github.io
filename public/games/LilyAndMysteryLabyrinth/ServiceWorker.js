5a91410a8452ce7692e726a22737fc185d937057
junya005 <junya005.65+dev@gmail.com>
2026-09-05 11:32:45 +0900

    [Add] Update Rougelike

--- public/games/LilyAndMysteryLabyrinth/ServiceWorker.js
+++ public/games/LilyAndMysteryLabyrinth/ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "DefaultCompany-RogueLikeLabyrinth-0.1.0";
+const cacheName = "DefaultCompany-RogueLikeLabyrinth-0.2.0";
 const contentToCache = [
     "Build/LilyAndMysteryLabyrinth.loader.js",
     "Build/LilyAndMysteryLabyrinth.framework.js",
